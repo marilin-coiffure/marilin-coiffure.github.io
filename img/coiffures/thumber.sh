8b67852d57deffe97c0f99ce3f0c5ebb2c28369d
chpache <christophe.pache@romande-energie.ch>
2018-08-31 13:42:42 +0200

    fix img orientation

--- img/coiffures/thumber.sh
+++ img/coiffures/thumber.sh
@@ -1,1 +1,5 @@
-for i in img-*.jpg; do convert $i -resize '30000@' thumb_$i; done
+for i in img-*.jpg; do
+  convert $i -resize '30000@' thumb_$i;
+  jhead -autorot $i
+  jhead -autorot thumb_$i
+done
